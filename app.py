9e38f18038d49407f44c4fcb49ee30634f3570d4
Radhika Amashi <54140246+amashiradhika@users.noreply.github.com>
2024-11-15 09:34:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from ragweb import get_answer  
 
@@ -17,5 +18,6 @@ def chat():
     return jsonify({"response": answer})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
